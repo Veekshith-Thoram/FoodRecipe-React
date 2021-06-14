d85826d36df6c9fb996b97b4dd14cf82532099e7
Veekshith-Thoram <veekshiththoram@gmail.com>
2021-06-15 01:30:07 +0530

    initial commit

---
+++ recipe-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Pushpa</h1>
+      <h1>Eyy Pushpa I am Love You!!</h1>
+      <h1>Kshminchu Pushpa please!!!</h1>
+    </div>
+  );
+}
+
+export default App;
