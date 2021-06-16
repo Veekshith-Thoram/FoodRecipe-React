f9fd25eda133819c780e3a2ba587b4c0934c6d94
Veekshith-Thoram <veekshiththoram@gmail.com>
2021-06-16 22:17:40 +0530

    updated with the empty state in query

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("chicken");
+  const [query, setQuery] = useState("");
   useEffect(()=>{
     getResponse();
   },[query])
